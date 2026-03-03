5a376848adfbaff41b0906ea23c66c3320e52540
Elias Cardenas <elias.cardenas0501@gmail.com>
2026-03-02 20:52:47 -0500

    Parte 1

---
+++ cajero electronico.py
@@ -0,0 +1,11 @@
+Saldo = 1000 #Saldo inicial
+
+print("Cajero electrónico")
+print("Seleccione su operación:")
+print("1.Consultar saldo")
+print("2.Pagos")
+print("3.Retiro")
+print("4.Deposito")
+print("5.Salir")
+    
+Opcion = int(input("Escoja una opción \n"))

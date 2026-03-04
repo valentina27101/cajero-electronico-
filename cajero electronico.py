6e7a9758cf7a5fcbba3921a5907eecccc64f792f
valentina27101 <pachecovalentinap@gmail.com>
2026-03-04 17:30:26 -0500

    agregue las funciones, faltan algunas

--- cajero electronico.py
+++ cajero electronico.py
@@ -9,3 +9,25 @@ print("4.Deposito")
 print("5.Salir")
     
 Opcion = int(input("Escoja una opción \n"))
+
+if Opcion == 1:
+    print ("su saldo actual es: ", Saldo)
+
+elif Opcion == 2: 
+    pago = float(input("ingrese el valor a pagar: "))
+    if pago <= Saldo:
+        Saldo -= pago 
+        print("pago realizado con exito.")
+        print("su nuevo saldo es: ", Saldo)
+
+    else:
+        print("saldo insuficiente.")
+    
+elif Opcion == 3:
+    retiro = float(input("Ingrese el valor a retirar: "))
+    if retiro <= Saldo:
+        Saldo -= retiro
+        print("Retiro exitoso.")
+        print("Su nuevo saldo es:", Saldo)
+    else:
+        print("Saldo insuficiente.")
